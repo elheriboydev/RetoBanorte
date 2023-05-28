8c6544490c51e2af0cfe56751e99bf63efb3b5b8
elheriboydev <103453101+elheriboydev@users.noreply.github.com>
2023-05-28 00:16:09 -0600

    Add files via upload

---
+++ Banorte3/Banorte3/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Banorte3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Banorte3.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("GetHome")]
+        public IActionResult GetHome()
+        {
+            List<int> intList = new List<int>();
+            intList.Add(1);
+            intList.Add(2);
+            intList.Add(3);
+            HomeResponse response = new HomeResponse()
+            {
+                HomeElements = intList
+            };
+            return new ObjectResult(response);
+        }
+    }
+}
---
+++ Banorte3/Banorte3/Models/HomeResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Banorte3.Models
+{
+    public class HomeResponse
+    {
+        [JsonPropertyName("homeElements")]
+        public List<int> HomeElements { get; set; }
+    }
+}
